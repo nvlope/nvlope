09cde286d6355c800968eefb22e14887a300249a
Cody <cody@codyq.dev>
2022-10-04 17:12:22 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![warn(
+    clippy::wildcard_imports,
+    clippy::string_add,
+    clippy::string_add_assign,
+    clippy::manual_ok_or,
+    unused_lifetimes
+)]
+
+fn main() {
+    println!("Hello, world!");
+}
